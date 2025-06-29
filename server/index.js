7874b96a34f300e648aae494ac1cd273c727011a
Tarun raj naik <74200794+tarun730@users.noreply.github.com>
2025-06-30 00:30:14 +0530

    fix

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
+import cors from 'cors';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 
